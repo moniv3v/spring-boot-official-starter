9455e48af9505ea36163284fec3a5886b91509ce
Monique <moniquechan1220@gmail.com>
2018-11-29 01:46:32 +0800

    Create UsersControllerTest

--- src/test/java/com/oocl/web/sampleWebApp/controller/UsersControllerTest.java
+++ src/test/java/com/oocl/web/sampleWebApp/controller/UsersControllerTest.java
@@ -1,16 +1,40 @@
 package com.oocl.web.sampleWebApp.controller;
 
 import org.apache.catalina.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.List;
 
-@RestController
-@RequestMapping("/users")
-public class UsersControllerTest {
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UsersControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultLocation() throws Exception {
+        this.mockMvc.perform(post("/users")).andDo(print()).andExpect(status().isCreated())
+                .andExpect(header().string("location", "/users/1"));
+    }
 
 }
